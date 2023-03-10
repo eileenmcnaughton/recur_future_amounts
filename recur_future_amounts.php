268e6e339e1eb69c6bdcd324d5e7ae7a0b630484
Eileen McNaughton <emcnaughton@wikimedia.org>
2023-03-10 15:24:58 +1300

    Shell extension

---
+++ recur_future_amounts.php
@@ -0,0 +1,61 @@
+<?php
+
+require_once 'recur_future_amounts.civix.php';
+// phpcs:disable
+use CRM_RecurFutureAmounts_ExtensionUtil as E;
+// phpcs:enable
+
+/**
+ * Implements hook_civicrm_config().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_config/
+ */
+function recur_future_amounts_civicrm_config(&$config): void {
+  _recur_future_amounts_civix_civicrm_config($config);
+}
+
+/**
+ * Implements hook_civicrm_install().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_install
+ */
+function recur_future_amounts_civicrm_install(): void {
+  _recur_future_amounts_civix_civicrm_install();
+}
+
+/**
+ * Implements hook_civicrm_enable().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_enable
+ */
+function recur_future_amounts_civicrm_enable(): void {
+  _recur_future_amounts_civix_civicrm_enable();
+}
+
+// --- Functions below this ship commented out. Uncomment as required. ---
+
+/**
+ * Implements hook_civicrm_preProcess().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_preProcess
+ */
+//function recur_future_amounts_civicrm_preProcess($formName, &$form): void {
+//
+//}
+
+/**
+ * Implements hook_civicrm_navigationMenu().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_navigationMenu
+ */
+//function recur_future_amounts_civicrm_navigationMenu(&$menu): void {
+//  _recur_future_amounts_civix_insert_navigation_menu($menu, 'Mailings', [
+//    'label' => E::ts('New subliminal message'),
+//    'name' => 'mailing_subliminal_message',
+//    'url' => 'civicrm/mailing/subliminal',
+//    'permission' => 'access CiviMail',
+//    'operator' => 'OR',
+//    'separator' => 0,
+//  ]);
+//  _recur_future_amounts_civix_navigationMenu($menu);
+//}
